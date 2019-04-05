6b7e144cf4920aa6372e27d534472d2efe5b9d3f
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-05 13:53:17 +0200

    created navbar component (side-nav), added future section ids for sections

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -8,9 +8,10 @@ class NavBar extends Component {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
                 <a className="navbar-brand js-scroll-trigger" href="#page-top">
-                    <span className="d-block d-lg-none">Clarence Taylor</span>
+                    <span className="d-block d-lg-none">DartCc</span>
                     <span className="d-none d-lg-block">
-                        <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg"
+                        <img className="img-fluid img-profile rounded-circle mx-auto mb-2"
+                             src="../../public/whitedart.png"
                              alt=""/>
                     </span>
                 </a>
