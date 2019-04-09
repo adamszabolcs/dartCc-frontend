7dd67ef87cef06d1334c43ed4b16d60694995376
adamszabolcs <szabolcs.adam91@gmail.com>
2019-04-09 15:52:33 +0200

    after send information to backend, call toggleNavbar method, goes to board

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -18,15 +18,18 @@ class Input extends Component {
                 "Content-Type": "application/json",
             },
             body: players,
-        })
+        });
+        this.props.toggleNavbar();
     };
+
+
 
     render() {
         return (
             <section className="resume-section d-flex" id="letsplay" style={{padding: "5rem"}}>
                 <div className="text-center">
                     <h1>LET'S PLAY!</h1>
-                    <form method="post">
+                    <form>
                         <input className="form-control"
                                type="text" name="p1name" value={this.props.playerOne.name}
                                placeholder="Player 1 name"
@@ -36,10 +39,9 @@ class Input extends Component {
                                type="text" name="p2name" value={this.props.playerTwo.name}
                                placeholder="Player 2 name"
                                onChange={this.props.getPlayerName}/>
-                               <br/>
-                        <button className="btn btn-primary"
-                                type="submit" onClick={this.createGame}><a href="#board"> Let's
-                            play!</a></button>
+                        <br/>
+                        <a className="btn btn-primary" onClick={this.createGame} href="#board"> Let's
+                            play!</a>
                     </form>
                 </div>
             </section>
